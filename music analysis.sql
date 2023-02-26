7d0323c111b9766b7f0ceeed8224eaaa57ebf8cf
R3kha05 <118958249+R3kha05@users.noreply.github.com>
2023-02-26 13:07:32 -0600

    Update music analysis.sql

--- music analysis.sql
+++ music analysis.sql
@@ -1,6 +1,18 @@
 select * from album
+select * from employee
+select * from artist
+select * from customer
+select * from genre
+select * from invoice
+select * from invoice_line
+select * from media_type
+select * from playlist
+select * from playlist_track
+select * from track
+
 --Q1: Who is the senior most employee based on job title? */
 select top 1 * from [dbo].[employee] order by levels desc
+
 --/* Q2: Which countries have the most Invoices? */
 select max(billing_state) as country from invoice 
 select * from invoice where billing_country=(select max(billing_country) as country from invoice )
@@ -34,7 +46,7 @@ group by c.customer_id,name
 order  by total_invoice
 --/* Question Set 2 - Moderate */
 
-/* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+/* Q6: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
 Return your list ordered alphabetically by email starting with A. */
 select * from customer
 select * from album where title =Rock Music
@@ -47,7 +59,7 @@ join track as t on l.track_id=t.track_id
 join genre as g on t.genre_id=g.genre_id
 where g.name like 'Rock'
 order by c.email 
---/* Q2: Let's invite the artists who have written the most rock music in our dataset. 
+--/* Q7: Let's invite the artists who have written the most rock music in our dataset. 
 --Write a query that returns the Artist name and total track count of the top 10 rock bands. */
 select * from artist
 select  distinct top 10 a.name as artist_name,count(a.artist_id) as total_track_count,g.name
@@ -58,23 +70,17 @@ join genre as g on t.genre_id=g.genre_id
 where g.name like 'Rock'
 group by a.name,g.name
 order by total_track_count desc
---/* Q3: Return all the track names that have a song length longer than the average song length. 
+--/* Q8: Return all the track names that have a song length longer than the average song length. 
 --Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */--
 select * from track
 select name, milliseconds from track where milliseconds >(select AVG(milliseconds)from track)
 order by milliseconds desc
 
---/* Question Set 3 - Advance */
 
-
-
--- We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
+--Q9 We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
 --with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where 
 --the maximum number of purchases is shared return all Genres. */
 
-/* Steps to Solve:  There are two parts in question- first most popular music genre and second need data at country level. */
-
-/* Method 1: Using CTE */
 with popular_music as (
 	select i.billing_country, count(l.quantity)as purchase,g.name,g.genre_id,
 	ROW_NUMBER() over(partition by i.billing_country order by count(l.quantity) desc ) as row
@@ -87,14 +93,10 @@ with popular_music as (
 
 select * from popular_music where  row <=1
 
-/* Write a query that determines the customer that has spent the most on music for each country. 
+/*Q10. Write a query that determines the customer that has spent the most on music for each country. 
 Write a query that returns the country along with the top customer and how much they spent. 
 For countries where the top amount spent is shared, provide all customers who spent this amount. */
 
-/* Steps to Solve:  Similar to the above question. There are two parts in question- 
-first find the most spent on music for each country and second filter the data for respective customers. */
-
-/* Method 1: using CTE */
 
 WITH country_wise as (
 	select c.customer_id,c.first_name,c.last_name,i.billing_country, sum(i.total)as total_spend,
